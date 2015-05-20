d96013c393a0a206c8d9ea73a23ff44d834523b9
Nathan Bergey <nathan.bergey@gmail.com>
2015-05-20 10:54:06 -0700

    clean up style in PID code

--- simulation/PIDcontroller.py
+++ simulation/PIDcontroller.py
@@ -1,45 +1,42 @@
-import random #for random values
-
-
-#for roll rate
-def setupRollRatePID(Kd,Ki,Kp,setPoint):
-    #set up PID controller
-    p=PIDController(Kp,Ki,Kd)
-    p.setTarget(setPoint)
-    return p
-
 class PIDController:
-    def __init__(self,p,i,d):
-        self.kP=p
-        self.kI=i
-        self.kD=d
-        self.target=0
+    """A simple PID Controller
+    """
 
-        self.lastError=0
-        self.integrator=0
+    def __init__(self, p, i, d):
+        self.kP = p
+        self.kI = i
+        self.kD = d
+        self.target = 0
+        self.lastError = 0
+        self.integrator = 0
 
-    def setTarget(self,newTarget):
-        self.target=newTarget
-        self.integrator=0
-
-    def step(self,currentValue):
+    def setTarget(self, newTarget):
+        """Set the target (set point) for the PID to track
         """
-        Calculates the error and derives a desired output value.
+
+        self.target = newTarget
+        self.integrator = 0
+
+    def step(self, currentValue):
+        """Calculates the error and derives a desired output value.
         """
+
         # determine the error by simply looking at the difference between
         # current value and target value.
-        error=currentValue-self.target
+        error = currentValue - self.target
 
         # Build the output by summing the contributions of the
         # proportional, integral, and derivative models.
-        output= (self.kP * error
-                 + self.kI * self.integrator
-                 + self.kD * (error - self.lastError)
-                 )
+        proportional = self.kP * error
+        integral = self.kI * self.integrator
+        derivative = self.kD * (error - self.lastError)
+
+        output = proportional + integral + derivative
 
         # Remember the error for the derivative model
-        self.lastError=error
+        self.lastError = error
+
         # Add the error to the integral model
-        self.integrator+=error
+        self.integrator += error
 
         return output
--- simulation/rollsim.py
+++ simulation/rollsim.py
@@ -3,9 +3,9 @@ import lv2
 import numpy
 from scipy.signal import resample
 import matplotlib.pyplot as plt
-import math
 import random
-import PIDcontroller as controller
+from PIDcontroller import PIDController
+
 
 # define things
 or_sample_rate = 100        # Hz
@@ -14,18 +14,20 @@ adis_sample_rate = 819.2    # Hz
 # Read in from open rocket
 columns = numpy.loadtxt('./openrocket/launch-12.csv', delimiter=',', unpack=True)
 
-time        = columns[0]
-altitude    = columns[1]*1000
-velocity    = columns[4]
+time     = columns[0]
+altitude = columns[1]*1000  # km to meters
+velocity = columns[4]
 
 # resample data to ADIS timebase
 nsamples = (len(time) * adis_sample_rate)/or_sample_rate
-#print nsamples
 #time     = resample(time, nsamples)
 #altitude = resample(altitude, nsamples)
 #velocity = resample(velocity, nsamples)
 
-pid=controller.setupRollRatePID(10,1,0,0)
+pid = PIDController(10, 1, 0)
+pid.setTarget(0.0)
+
+
 angular_accel = []
 roll = [0]
 angle = [0]
@@ -33,16 +35,16 @@ for i, t in enumerate(time):
     correction = pid.step(roll[-1])
     print correction
     # TODO: Reverse look up for alpha (canard angle)
-    a = 0 # canard angle in degrees
-    a = lv2.servo(a, t) # request servo update canard angle 
+    a = 0                    # canard angle in degrees
+    a = lv2.servo(a, t)      # request servo update canard angle
     x = altitude[i]
     v = velocity[i]
     aa = lv2.angular_accel(a, x, v, t)
 
-    aa_offset = 50 # ang acc offset in degs/s^2
-    aa_rand = random.gauss(aa_offset, 50) # random ang acc on the rocket 
+    aa_offset = 50                          # ang acc offset in degs/s^2
+    aa_rand = random.gauss(aa_offset, 50)   # random ang acc on the rocket
 
-    aa += aa_rand # add random torque to rocket
+    aa += aa_rand                           # add random torque to rocket
     if i >= 1:
         r = roll[i-1] + aa/or_sample_rate
         roll.append(r)
@@ -50,11 +52,11 @@ for i, t in enumerate(time):
         angle.append(ang)
     angular_accel.append(aa)
 
-ax = plt.figure(figsize=(16,9))
+
+ax = plt.figure(figsize=(16, 9))
 plt.plot(time, angle, 'r-')
 plt.xlabel('Time [s]')
 plt.ylabel('Angle [o]')
-ax.axes[0].set_xlim([0,30])
+ax.axes[0].set_xlim([0, 30])
 plt.title("Roll Angle")
-#plt.legend()
 plt.show()
