57b66d5c3349153b6a1e3c958bb44e81361a9e2f
wrh2 <wrh2@pdx.edu>
2015-05-19 21:13:24 -0700

    modified rollsim

--- simulation/rollsim.py
+++ simulation/rollsim.py
@@ -3,6 +3,8 @@ import lv2
 import numpy
 from scipy.signal import resample
 import matplotlib.pyplot as plt
+import math
+import random
 
 # define things
 or_sample_rate = 100        # Hz
@@ -27,16 +29,18 @@ angular_accel = []
 roll = [0]
 angle = [0]
 for i, t in enumerate(time):
-    a = 0
-    if t > 5 and t < 8:
-        a = 1
-    elif t > 12 and t < 15:
-        a = -1
-    else:
-        a = 0
+    # TODO: PID goes here
+    
+    a = 0 # canard angle in degrees
+    a = lv2.servo(a, t) # request servo update canard angle 
     x = altitude[i]
     v = velocity[i]
     aa = lv2.angular_accel(a, x, v, t)
+
+    aa_offset = 50 # ang acc offset in degs/s^2
+    aa_rand = random.gauss(aa_offset, 50) # random ang acc on the rocket 
+
+    aa += aa_rand # add random torque to rocket
     if i >= 1:
         r = roll[i-1] + aa/or_sample_rate
         roll.append(r)
