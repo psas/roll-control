1af7545a469551bb7016ea8f6ed33936f6f213f5
Nathan Bergey <nathan.bergey@gmail.com>
2015-05-24 14:36:49 -0700

    roll simulation with PID control

--- simulation/rollsim.py
+++ simulation/rollsim.py
@@ -8,7 +8,7 @@ from PIDcontroller import PIDController
 
 
 # define things
-or_sample_rate = 100        # Hz
+or_sample_rate = 820.0      # Hz
 adis_sample_rate = 819.2    # Hz
 
 # Read in from open rocket
@@ -18,45 +18,72 @@ time     = columns[0]
 altitude = columns[1]*1000  # km to meters
 velocity = columns[4]
 
-# resample data to ADIS timebase
-nsamples = (len(time) * adis_sample_rate)/or_sample_rate
-#time     = resample(time, nsamples)
-#altitude = resample(altitude, nsamples)
-#velocity = resample(velocity, nsamples)
+# only care about launch to apogee
+begin = 500
+apogee = 25000
+time = time[begin:apogee]
+altitude = altitude[begin:apogee]
+velocity = velocity[begin:apogee]
 
-pid = PIDController(10, 1, 0)
+# Set up PID
+pid = PIDController(p=10.0, i=0.0, d=0.0)
 pid.setTarget(0.0)
 
-
+# simulate roll
 angular_accel = []
 roll = [0]
 angle = [0]
+alpha = []
+pids =[]
 for i, t in enumerate(time):
-    correction = pid.step(roll[-1])
-    print correction
-    # TODO: Reverse look up for alpha (canard angle)
-    a = 0                    # canard angle in degrees
-    a = lv2.servo(a, t)      # request servo update canard angle
+
+    # current time step:
     x = altitude[i]
     v = velocity[i]
+    r = roll[-1]
+    correction = pid.step(r)
+    a = lv2.estimate_alpha(correction, x, v, t)
+    a = lv2.servo(a, t)
     aa = lv2.angular_accel(a, x, v, t)
 
-    aa_offset = 50                          # ang acc offset in degs/s^2
-    aa_rand = random.gauss(aa_offset, 50)   # random ang acc on the rocket
+    if t > 7 and t < 7.1:
+        aa += 100
 
-    aa += aa_rand                           # add random torque to rocket
-    if i >= 1:
-        r = roll[i-1] + aa/or_sample_rate
-        roll.append(r)
-        ang = angle[i-1] + r/or_sample_rate
-        angle.append(ang)
+    #aa_offset = 10                          # ang acc offset in degs/s^2
+    #aa_rand = random.gauss(aa_offset, 20)   # random ang acc on the rocket
+
+    #aa += aa_rand                           # add random torque to rocket
+    #aa = 1.0
+
+    # next time step:
+    r += aa/or_sample_rate
+    roll.append(r)
+    ang = angle[-1] + r/or_sample_rate
+    angle.append(ang)
+    alpha.append(a)
     angular_accel.append(aa)
+    pids.append(correction)
 
+# chart
+fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=4, sharex=True)
+ax0.plot(time, roll[1:], 'r-')
+ax0.set_title('Roll Rate')
+ax0.set_ylabel(r'Roll Rate [${}^0/s$]')
+ax0.set_ylim([-400,400])
 
-ax = plt.figure(figsize=(16, 9))
-plt.plot(time, angle, 'r-')
-plt.xlabel('Time [s]')
-plt.ylabel('Angle [o]')
-ax.axes[0].set_xlim([0, 30])
-plt.title("Roll Angle")
+ax1.plot(time, angular_accel, 'r-')
+ax1.set_title('Roll Accel')
+ax1.set_ylabel(r'$\omega$ [${}^0/s/s$]')
+ax1.set_ylim([-1000,1000])
+
+ax2.plot(time, pids)
+ax2.set_title('Correction')
+ax2.set_ylabel(r'Correction')
+
+ax3.plot(time, alpha)
+ax3.set_title('Alpha')
+ax3.set_ylabel(r'Alpha [${}^0$]')
+ax3.set_xlabel(r'Time [$s$]')
+
+plt.savefig('out.png', dpi=200)
 plt.show()
