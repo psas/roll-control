ca8dbcd28c8a95226255987b4b138b2bc7cbe522
wrh2 <wrh2@pdx.edu>
2015-07-03 23:54:28 -0700

    better random model, some decent PI performance

--- simulation/rollsim.py
+++ simulation/rollsim.py
@@ -26,7 +26,7 @@ altitude = altitude[begin:apogee]
 velocity = velocity[begin:apogee]
 
 # Set up PID
-pid = PIDController(p=10.0, i=0.01, d=0.1)
+pid = PIDController(p=50, i=0.009, d=0)
 pid.setTarget(0.0)
 
 # simulate roll
@@ -35,12 +35,16 @@ roll = [0]
 angle = [0]
 alpha = []
 pids =[]
+rand_stuff = []
+
 for i, t in enumerate(time):
 
     # current time step:
     x = altitude[i]
     v = velocity[i]
     r = roll[-1]
+
+    #correction = pid.step(angle[-1])
     correction = pid.step(r)
     a = lv2.estimate_alpha(correction, x, v, t)
     a = lv2.servo(a, t)
@@ -51,8 +55,10 @@ for i, t in enumerate(time):
     #   aa += 100
 
     # random acceleration:
-    aa_offset = 80                          # ang acc offset in degs/s^2
+    #aa_offset = 80                          # ang acc offset in degs/s^2
+    aa_offset = v/2.0
     aa_rand = random.gauss(aa_offset, 20)   # random ang acc on the rocket
+    rand_stuff.append(aa_rand)
     aa += aa_rand                           # add random torque to rocket
 
     # next time step:
@@ -65,25 +71,29 @@ for i, t in enumerate(time):
     pids.append(correction)
 
 # chart
-fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=4, sharex=True, figsize=(16,9))
-ax0.plot(time, roll[1:], 'r-')
-ax0.set_title('Roll Rate')
-ax0.set_ylabel(r'Roll Rate [${}^0/s$]')
-ax0.set_ylim([-400,400])
+fig, (ax0, ax1, ax2, ax3, ax4) = plt.subplots(nrows=5, sharex=True, figsize=(16,9))
+ax0.plot(time, angle[1:], 'g-')
+ax0.set_title('Rocket Angle')
+ax0.set_ylabel(r'Angle [${}^0$]')
 
-ax1.plot(time, angular_accel, 'r-')
-ax1.set_title('Roll Accel')
-ax1.set_ylabel(r'$\omega$ [${}^0/s/s$]')
-ax1.set_ylim([-1000,1000])
+ax1.plot(time, roll[1:], 'r-')
+ax1.set_title('Roll Rate')
+ax1.set_ylabel(r'Roll Rate [${}^0/s$]')
+ax1.set_ylim([-5,10])
 
-ax2.plot(time, pids)
-ax2.set_title('Correction')
-ax2.set_ylabel(r'Correction')
+ax2.plot(time, angular_accel, 'r-')
+ax2.set_title('Roll Accel')
+ax2.set_ylabel(r'$\omega$ [${}^0/s/s$]')
+ax2.set_ylim([-100,100])
 
-ax3.plot(time, alpha)
-ax3.set_title('Alpha')
-ax3.set_ylabel(r'Alpha [${}^0$]')
-ax3.set_xlabel(r'Time [$s$]')
+ax3.plot(time, pids)
+ax3.set_title('Correction')
+ax3.set_ylabel(r'Correction')
+
+ax4.plot(time, alpha)
+ax4.set_title('Alpha')
+ax4.set_ylabel(r'Alpha [${}^0$]')
+ax4.set_xlabel(r'Time [$s$]')
 
 plt.savefig('out.png', dpi=200)
 plt.show()
